ec4c82d314b57efe6bfaf1e0cba3a4058e406816
alirezass <alirezakhosraviss1526@gmail.com>
2026-06-02 22:40:06 +0330

    set state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 function App() {
+    const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
